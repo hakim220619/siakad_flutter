29948145689c0cb73bb45abcc7ec8d62ab460172
Dani lukman <danilukman2206@gmail.com>
2024-01-03 23:19:43 +0700

    asd

--- lib/login/service/service_page.dart
+++ lib/login/service/service_page.dart
@@ -17,7 +17,6 @@ class HttpService {
     http.Response response = await _client
         .post(_loginUrl, body: {"email": email, "password": password});
 
-    // print(response.statusCode);
     if (response.statusCode == 200) {
       // ignore: non_constant_identifier_names
 // test@example.com
